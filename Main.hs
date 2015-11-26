28ca7599798c9afae883c9de0774370ccdad1272
paldepind <simonfv@gmail.com>
2015-11-26 09:19:39 +0100

    Parser – Support parentheses & escaped strings

--- Main.hs
+++ Main.hs
@@ -10,7 +10,9 @@ data LispVal
   | DottedList [LispVal] LispVal
   | Number Integer
   | String String
+  | Char Char
   | Bool Bool
+  deriving (Show)
 
 test = [1]
 
@@ -20,15 +22,21 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
+escaped :: Char -> Char -> Parser Char
+escaped esc as = try $ string ('\\':esc:[]) >> return as
+
+parseChar :: Parser LispVal
+parseChar = string "#\\" >> anyChar >>= (return . Char)
+
 parseString :: Parser LispVal
 parseString =
   do char '"'
-     x <- many (noneOf "\"")
+     x <- many (escaped 'n' '\n' <|> escaped '"' '"' <|> noneOf "\"")
      char '"'
      return $ String x
      
 parseAtom =
-  do first <- letter <|> symbol
+  do first <- try letter <|> symbol
      rest <- many (letter <|> digit <|> symbol)
      let atom = first:rest
      return $ case atom of
@@ -37,16 +45,42 @@ parseAtom =
                 _    -> Atom atom
                 
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = (Number . read) <$> many1 digit
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseParens :: Parser LispVal
+parseParens = do char '('
+                 x <- try parseList <|> parseDottedList
+                 char ')'
+                 return x
+
+parseDottedList :: Parser LispVal
+parseDottedList =
+  do head <- parseExpr `endBy` spaces
+     tail <- char '.' >> spaces >> parseExpr
+     return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted =
+  do char '\''
+     x <- parseExpr
+     return $ List [Atom "quote", x]
 
 parseExpr :: Parser LispVal
-parseExpr = parseAtom <|> parseString <|> parseNumber
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseChar
+         <|> parseQuoted
+         <|> parseNumber
+         <|> parseParens
 
 readExpr :: String -> String
 readExpr input =
   case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+    Right val -> "Found value" ++ show val
 
 main :: IO ()
 main =
